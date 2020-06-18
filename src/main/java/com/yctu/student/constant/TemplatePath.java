bbdde35d74e7d2a46ee48d3d66f8fd81cd7b6df8
Cpt-LC <1579029588@qq.com>
2020-06-18 18:37:12 +0800

    -update

--- src/main/java/com/yctu/student/constant/TemplatePath.java
+++ src/main/java/com/yctu/student/constant/TemplatePath.java
@@ -16,8 +16,14 @@ public class TemplatePath {
     /** 管理员查看学生列表页面 */
     public static final String ADMIN_LIST_STUDENT = "admin/list-student";
 
+    /** 管理员查看老师列表页面 */
+    public static final String ADMIN_LIST_TEACHER = "admin/list-teacher";
+
     /** 管理员修改学生信息页面 */
     public static final String ADMIN_UPDATE_STUDENT = "admin/update-student";
+
+    /** 管理员修改学生信息页面 */
+    public static final String ADMIN_UPDATE_TEACHER = "admin/update-teacher";
 
     /** 登录页面 */
     public static final String LOGIN = "login";
@@ -27,6 +33,9 @@ public class TemplatePath {
 
     /** 管理员添加学生信息页面 */
     public static final String ADMIN_ADD_STUDENT = "admin/add-student";
+
+    /** 管理员添加老师信息页面 */
+    public static final String ADMIN_ADD_TEACHER = "admin/add-teacher";
 
     /** 管理员主页页面 */
     public static final String ADMIN_MAIN = "admin/main";
